6bae8184982312aa402fd19cf9ed2195a9cf360d
ssskram <paul.marks@pittsburghpa.gov>
2018-09-26 12:10:54 -0400

    take 2

--- wwwroot/dist/main-client.js
+++ wwwroot/dist/main-client.js
@@ -59,7 +59,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "d0faab2c19b9ff881248"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "7ac0475a940997c51c90"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
