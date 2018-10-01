24d806facd5dac9c75cd2455c4a9c386d51c13e2
ssskram <paul.marks@pittsburghpa.gov>
2018-10-01 16:44:33 -0400

    drawdown server endpoints

--- Controllers/Drawdowns.cs
+++ Controllers/Drawdowns.cs
@@ -22,7 +22,7 @@ namespace pghworks.Controllers {
         }
         HttpClient client = new HttpClient ();
 
-        public class Tag {
+        public class Drawdown {
             public string drawdownID { get; set; }
             public string parentID { get; set; }
             public string parentType { get; set; }
@@ -30,30 +30,32 @@ namespace pghworks.Controllers {
             public string drawdownAmount { get; set; }
             public string drawdownType { get; set; }
             public string notes { get; set; }
+            public string spID { get; set; }
         }
 
         // GET
         [HttpGet ("[action]")]
-        public object loadPhases () {
-            List<Tag> AllTags = new List<Tag> ();
-            string tags = getTags ().Result;
-            dynamic tagsObject = JObject.Parse (tags) ["value"];
-            foreach (var item in tagsObject) {
-                Tag tg = new Tag () {
+        public object loadDrawdowns () {
+            List<Drawdown> AllDrawdowns = new List<Drawdown> ();
+            string drawdowns = getDrawdowns ().Result;
+            dynamic drawdownsObject = JObject.Parse (drawdowns) ["value"];
+            foreach (var item in drawdownsObject) {
+                Drawdown tg = new Drawdown () {
                     drawdownID = item.drawdownID,
                     parentID = item.parentID,
                     parentType = item.parentType,
                     fundID = item.fundID,
                     drawdownAmount = item.drawdownAmount,
                     drawdownType = item.drawdownType,
-                    notes = item.notes
-                };
-                AllTags.Add (tg);
-            }
-            return (AllTags);
-        }
+                    notes = item.notes,
+                    spID = item.Id
 
-        public async Task<string> getTags () {
+                };
+                AllDrawdowns.Add (tg);
+            }
+            return (AllDrawdowns);
+        }
+        public async Task<string> getDrawdowns () {
             string token = refreshtoken ().Result;
             client.DefaultRequestHeaders.Clear ();
             client.DefaultRequestHeaders.Add ("Accept", "application/json");
@@ -61,6 +63,57 @@ namespace pghworks.Controllers {
                 new AuthenticationHeaderValue ("Bearer", token);
             string listitems = await client.GetStringAsync ("https://cityofpittsburgh.sharepoint.com/sites/pghworks/_api/web/lists/GetByTitle('drawdowns')/items");
             return listitems;
+        }
+
+        // POST
+        [HttpPost ("[action]")]
+        public async Task addDrawdown ([FromBody] Drawdown model) {
+            string drawdownLoad = JsonConvert.SerializeObject (model);
+            string token = refreshtoken ().Result;
+            var sharepointUrl = "https://cityofpittsburgh.sharepoint.com/sites/pghworks/_api/web/lists/GetByTitle('drawdowns')/items";
+            client.DefaultRequestHeaders.Clear ();
+            client.DefaultRequestHeaders.Add ("X-HTTP-Method", "POST");
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue ("Basic", token);
+            string json = "{ '__metadata': {{ 'type': 'SP.Data.drawdownsItem' }}," + drawdownLoad + "}";
+            client.DefaultRequestHeaders.Add ("ContentLength", json.Length.ToString ());
+            try {
+                StringContent strContent = new StringContent (json);
+                strContent.Headers.ContentType = MediaTypeHeaderValue.Parse ("application/json;odata=verbose");
+                HttpResponseMessage response = client.PostAsync (sharepointUrl, strContent).Result;
+                response.EnsureSuccessStatusCode ();
+                var content = await response.Content.ReadAsStringAsync ();
+            } catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine (ex.Message);
+            }
+            await new log ().postLog (_userManager.GetUserName (HttpContext.User), "Post", "Drawdown", model.parentType, model.drawdownID);
+        }
+
+        // POST
+        [HttpPut ("[action]")]
+        public async Task updateDrawdown ([FromBody] Drawdown model) {
+            string drawdownLoad = JsonConvert.SerializeObject (model);
+            string token = refreshtoken ().Result;
+            var sharepointUrl = string.Format("https://cityofpittsburgh.sharepoint.com/sites/pghworks/_api/web/lists/GetByTitle('drawdowns')/items({0})", model.spID);
+            client.DefaultRequestHeaders.Clear ();
+            client.DefaultRequestHeaders.Add ("Accept", "application/json;odata=verbose");
+            client.DefaultRequestHeaders.Add ("X-RequestDigest", "form digest value");
+            client.DefaultRequestHeaders.Add ("X-HTTP-Method", "MERGE");
+            client.DefaultRequestHeaders.Add ("IF-MATCH", "*");
+            client.DefaultRequestHeaders.Authorization =
+                new AuthenticationHeaderValue ("Basic", token);
+            string json = "{ '__metadata': {{ 'type': 'SP.Data.drawdownsItem' }}," + drawdownLoad + "}";
+            client.DefaultRequestHeaders.Add ("ContentLength", json.Length.ToString ());
+            try {
+                StringContent strContent = new StringContent (json);
+                strContent.Headers.ContentType = MediaTypeHeaderValue.Parse ("application/json;odata=verbose");
+                HttpResponseMessage response = client.PostAsync (sharepointUrl, strContent).Result;
+                response.EnsureSuccessStatusCode ();
+                var content = await response.Content.ReadAsStringAsync ();
+            } catch (Exception ex) {
+                System.Diagnostics.Debug.WriteLine (ex.Message);
+            }
+            await new log ().postLog (_userManager.GetUserName (HttpContext.User), "Put", "Drawdown", model.parentType, model.drawdownID);
         }
 
         private async Task<string> refreshtoken () {
