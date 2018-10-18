8b4647f6aaba061c569e77dd9df360b0988b0716
ssskram <paul.marks@pittsburghpa.gov>
2018-10-17 22:28:42 -0400

    mobile table view, drawdowns

--- ClientApp/components/Reports/Project/Drawdowns.tsx
+++ ClientApp/components/Reports/Project/Drawdowns.tsx
@@ -177,7 +177,7 @@ export class ProgramsFunds extends React.Component<any, any> {
             }]
         } else {
             columns = [{
-                Header: 'Fund/Program',
+                Header: 'Fund',
                 accessor: 'fundID',
                 Cell: props => <div>{this.getFundName(props.value)}</div>
             }, {
@@ -189,12 +189,12 @@ export class ProgramsFunds extends React.Component<any, any> {
                 accessor: 'fundID',
                 Cell: props => <div>{this.getFundType(props.value)}</div>
             }, {
-                Header: 'Drawdown Amount',
+                Header: 'Drawdown',
+                accessor: 'drawdownType'
+            }, {
+                Header: 'Amount',
                 accessor: 'drawdownAmount',
                 Cell: props => <CurrencyFormat value={props.value} displayType={'text'} thousandSeparator={true} prefix={'$'} />
-            }, {
-                Header: 'Drawdown Type',
-                accessor: 'drawdownType'
             }, {
                 Header: 'Notes',
                 accessor: 'notes'
