18e5e3e88f658ecc69eb5b4e153704e2134837f0
ssskram <paul.marks@pittsburghpa.gov>
2018-12-03 17:24:34 -0500

    ditto

--- ClientApp/components/SiteTour/contents/submitFeedback.tsx
+++ ClientApp/components/SiteTour/contents/submitFeedback.tsx
@@ -5,7 +5,6 @@ export default class SubmitFeedback extends React.Component<any, any> {
     public render() {
         return <div>
             <h3>Questions? Feedback?</h3>
-            <h4>This site will continue to change according to your direction</h4>
             <h4><b>We need your feedback</b></h4>
             <h4>Click the link in the top right to share your thoughts</h4>
         </div>
